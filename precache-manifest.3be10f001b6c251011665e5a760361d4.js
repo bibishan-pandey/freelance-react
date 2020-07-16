501183ac1c1fa6597993d84e892b1b5fc83884d1
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-07-16 10:38:23 +0545

    Updates

---
+++ precache-manifest.3be10f001b6c251011665e5a760361d4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f2b09e88997c55a1764ee1e81d44ef",
+    "url": "/freelance-react/index.html"
+  },
+  {
+    "revision": "38b1829f11c340a44f82",
+    "url": "/freelance-react/static/css/2.2446d6d5.chunk.css"
+  },
+  {
+    "revision": "f4b6637c36ad293e03b3",
+    "url": "/freelance-react/static/css/main.a265fff5.chunk.css"
+  },
+  {
+    "revision": "38b1829f11c340a44f82",
+    "url": "/freelance-react/static/js/2.31ea794c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/freelance-react/static/js/2.31ea794c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b6637c36ad293e03b3",
+    "url": "/freelance-react/static/js/main.c93d8332.chunk.js"
+  },
+  {
+    "revision": "af46ce9c0994aa6ccede",
+    "url": "/freelance-react/static/js/runtime-main.411823bf.js"
+  },
+  {
+    "revision": "671e58ad99cc754022f4606c02957063",
+    "url": "/freelance-react/static/media/avatar-female.671e58ad.png"
+  },
+  {
+    "revision": "3c9e1e03c0120ce9cbb50cfdf96050be",
+    "url": "/freelance-react/static/media/avatar-male.3c9e1e03.png"
+  },
+  {
+    "revision": "37507abc83b59ae146af73a72a640059",
+    "url": "/freelance-react/static/media/background.37507abc.jpg"
+  },
+  {
+    "revision": "fa9a946bd72ad65487c22f1b7e6b27b6",
+    "url": "/freelance-react/static/media/bird_red.fa9a946b.png"
+  },
+  {
+    "revision": "a004ed37bbdab3bace5a4bec27de455b",
+    "url": "/freelance-react/static/media/bivishan.a004ed37.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/freelance-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/freelance-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/freelance-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/freelance-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/freelance-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "49e5e85de105af25802cc10bd945466f",
+    "url": "/freelance-react/static/media/freelancer-logo_red.49e5e85d.png"
+  },
+  {
+    "revision": "723594945c12aa7dcbfa2a4002ce1b59",
+    "url": "/freelance-react/static/media/iwanttohire-1.72359494.png"
+  },
+  {
+    "revision": "c433ccda563aea184816897eee112bb6",
+    "url": "/freelance-react/static/media/iwanttohire-2.c433ccda.png"
+  },
+  {
+    "revision": "3a2ec70bd97a6b674bdc3e6ea1c3f772",
+    "url": "/freelance-react/static/media/iwanttowork-1.3a2ec70b.png"
+  },
+  {
+    "revision": "cf7641ca7aab06a7ea3296309d446232",
+    "url": "/freelance-react/static/media/iwanttowork-2.cf7641ca.png"
+  },
+  {
+    "revision": "fb58115891355b02c2da11c7bdc33501",
+    "url": "/freelance-react/static/media/iwanttowork-3.fb581158.png"
+  }
+]);
